21fa82b7eb39e7fee0bff8805079a187c9bdffb1
Divy Pratap Singh Rathore <divypratap.work@gmail.com>
2026-01-02 09:15:15 +0530

    Added query to drop persons table

---
+++ Day02_DDL_Table_Operations/drop_persons_table.sql
@@ -0,0 +1,15 @@
+/*
+Problem:
+Delete the persons table from the database.
+
+Concepts used:
+DROP TABLE
+
+Approach:
+Use DROP TABLE to permanently remove the table and its data.
+
+Result:
+The persons table is removed from the database.
+*/
+
+DROP TABLE persons;
