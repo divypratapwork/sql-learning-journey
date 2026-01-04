c9651a785c8e9c05c1a24e7ae9f9610badbdd835
Divy Pratap Singh Rathore <divypratap.work@gmail.com>
2026-01-04 23:08:13 +0530

    Added query to delete all records from person table

---
+++ Day03_DML_Data_Manipulation/06_delete_all_data_from_person.sql
@@ -0,0 +1,17 @@
+/*
+Problem:
+Delete all records from the person table.
+
+Concepts used:
+DELETE, TRUNCATE (commented)
+
+Approach:
+Use DELETE to remove all rows from the table.
+
+Result:
+The person table becomes empty.
+*/
+
+DELETE FROM person;
+
+-- TRUNCATE TABLE person;
