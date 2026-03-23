c1fede58b1dbda9ffaa2955cb164bcc8051ff26f
Divy Pratap Singh Rathore <divypratap.work@gmail.com>
2026-03-23 21:19:01 +0530

    Create 04_drop_table.sql

---
+++ Day-02/04_drop_table.sql
@@ -0,0 +1,19 @@
+/*
+==================================================
+Query: Delete 'persons' Table
+==================================================
+
+Problem Statement:
+Delete the 'persons' table from the database.
+
+Concepts Used:
+- DROP TABLE
+
+Approach:
+Use DROP TABLE to permanently remove the table and all its data.
+
+Outcome:
+The 'persons' table is completely removed from the database.
+*/
+
+DROP TABLE persons;
