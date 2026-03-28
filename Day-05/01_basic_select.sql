1e68509010a32904b2353ea485d12d5abb016bea
Divy Pratap Singh Rathore <divypratap.work@gmail.com>
2026-03-28 19:36:34 +0530

    Create 01_basic_select.sql

---
+++ Day-05/01_basic_select.sql
@@ -0,0 +1,19 @@
+/*
+Problem:
+Retrieve all records from customers and orders separately.
+
+Concepts used:
+SELECT
+
+Approach:
+Run separate queries to understand both tables.
+
+Result:
+Displays all records from both tables.
+*/
+
+SELECT *
+FROM customers;
+
+SELECT *
+FROM orders;
